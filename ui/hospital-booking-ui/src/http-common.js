12096767375ab9cedf91ad20bfd4741f298f2aab
Salem Alawi <salemalowi@gmail.com>
2021-10-26 15:31:12 +0300

    fixing design

--- ui/hospital-booking-ui/src/http-common.js
+++ ui/hospital-booking-ui/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://localhost:8080/",
+    baseURL: "http://52.36.50.145:8444/",
     headers: {
         "Content-type": "application/json"
     }
